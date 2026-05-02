28ebec558e10aabf5cb4fab5238c475a8d59927f
Juarez <jardejuarez@gmail.com>
2026-05-02 09:11:49 -0400

    subiendo ejemplo

---
+++ ejemplos/ejemplo01.py
@@ -0,0 +1,18 @@
+#ejemplo 01 de pythom para formacion complementaria
+x = 5
+y = 10
+
+print ("el valor de 5 * 10 es: ", x*y )
+print ("Realizado")
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+
+print ("El nombre es: " + nombre + " " + apellido )
+
+edad = int(input("Ingrese su edad: "))
+
+if (edad > 18):
+    print("Pase con confianza")
+else:
+    print("Rodaste compadre")
